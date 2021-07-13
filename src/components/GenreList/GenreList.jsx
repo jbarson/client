3d4e0d14ebd988aa68a5eb6175a00ea2f076d7a0
Jon Barson <jbarson@gmail.com>
2021-07-13 09:47:27 -0400

    t/s

--- src/components/GenreList/GenreList.jsx
+++ src/components/GenreList/GenreList.jsx
@@ -5,13 +5,14 @@ import './GenreList.scss';
 
 function GenreList() {  
   const [genres, setGenres] = useState([])
-
+  console.log(process.env)
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}/api/genres`)
       .then(res => {
         console.log(res.data)
         setGenres(res.data)
       })
+      .catch(console.error)
   }, [])
 
   document.title = "Welcome to Lauren's Bookstore"
